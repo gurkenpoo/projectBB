b6c38a48b01e3f02df7814997565a6d2f3349c12
gurkenpoo <bastianpoo@gmail.com>
2024-09-19 19:31:29 -0300

    testing triggers/sounds

---
+++ scripts/trigger_area.gd
@@ -0,0 +1,24 @@
+extends Area3D
+
+@onready var ambienceSound : AudioStreamPlayer = $"../AudioStreamPlayer"
+@onready var timer : Timer = $"../Timer"
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_body_entered(body):
+	if body.name == "CharacterBody3D":
+		ambienceSound.play();
+		print("reproduciendo sonido...")
+		print(body.name)
+
+
+func _on_timer_timeout() -> void:
+	pass
+	
