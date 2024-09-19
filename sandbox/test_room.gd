8c383081c773d795eb84136f0ad58697e301d2cb
gurkenpoo <bastianpoo@gmail.com>
2024-09-19 20:24:37 -0300

    add sounds and test_rooms

---
+++ sandbox/test_room.gd
@@ -0,0 +1,12 @@
+extends Node3D
+
+@onready var atmosound : AudioStreamPlayer = $AtmoSound
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	atmosound.play();
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
