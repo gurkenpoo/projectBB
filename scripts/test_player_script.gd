8e2f5ae9aa797fcf8319f61c2aa6f5b493474929
gurkenpoo <bastianpoo@gmail.com>
2024-09-19 18:06:33 -0300

    fix merge conflict

--- scripts/test_player_script.gd
+++ scripts/test_player_script.gd
@@ -10,7 +10,7 @@ var isHoldingObject : bool = false;
 var heldObject = null;
 
 
-const SPEED = 1.0;
+const SPEED = 5.0;
 const JUMP_VELOCITY = 2.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -31,9 +31,13 @@ func _unhandled_input(event):
 	if Input.is_action_just_pressed("action"):
 		print("hola")
 		
+	if Input.is_action_pressed("ui_cancel"):
+		print("Saliendo...")
+		get_tree().quit()
+		
 	pass
 		
-func _physics_process(delta):
+func _physics_process(delta) -> void:
 	# Añadir gravedad.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -43,7 +47,7 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 
 	# Obtener dirección de input y manejar movimiento/desaceleración.
-	var input_dir = Input.get_vector("izquierda", "derecha", "arriba", "abajo")
+	var input_dir = Input.get_vector("arriba", "abajo", "derecha", "izquierda")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
