b67e7932a4193195640398e4568ae01c9eb9a061
gurkenpoo <bastianpoo@gmail.com>
2024-09-24 21:16:34 -0300

    fix door bug

--- scripts/player_script.gd
+++ scripts/player_script.gd
@@ -1,20 +1,5 @@
 extends CharacterBody3D
 
-var speed
-const WALK_SPEED : float = 5.0
-const SPRINT_SPEED  : float = 8.0
-const JUMP_VELOCITY : float = 4.5
-const SENSITIVITY : float = 0.0027
-
-
-#FOV variable
-const BASE_FOV = 75.0
-const FOV_CHANGE = 1.5
-
-#Drag door
-var isHoldingObject = false
-var heldObject = null
-
 @onready var head : Node3D = $head
 @onready var camera : Camera = $head/Camera3D
 @onready var raycast : RayCast3D = $head/Camera3D/RayCast3D
@@ -23,8 +8,24 @@ var heldObject = null
 
 @export_category("Head Bob")
 @export var BOB_FREQ : int = 2
-@export var BOB_AMP = 0.08
-@export var t_bob = 0
+@export var BOB_AMP : float = 0.08
+@export var t_bob : float= 0
+
+var speed
+const WALK_SPEED : float = 2.0
+const SPRINT_SPEED  : float = 8.0
+const JUMP_VELOCITY : float = 2.5
+const SENSITIVITY : float = 0.0027
+
+
+#FOV variable
+const BASE_FOV = 65.0
+const FOV_CHANGE = 1.5
+
+#Drag door
+var isHoldingObject = false
+var heldObject = null
+
 
 func _ready() -> void:
 	bloodyCursor.hide()
@@ -84,12 +85,20 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	
 	if Input.is_action_just_pressed("interact"):
+		print("agarrar")
 		interectWithDoor()
 		
 	elif Input.is_action_just_released("interact"):
 		print("soltar")
 		releaseObject()
+		
 	maintainInteraction()
+	
+	for col_idx in get_slide_collision_count():
+		var col := get_slide_collision(col_idx)
+		if col.get_collider() is RigidBody3D:
+			col.get_collider().apply_central_impulse(-col.get_normal() * 0.3)
+			col.get_collider().apply_impulse(-col.get_normal() * 0.01, col.get_position())
 	
 func _headbob(time) -> Vector3:
 	var pos = Vector3.ZERO
@@ -106,6 +115,7 @@ func interectWithDoor():
 			if collider.is_in_group("door"):
 				isHoldingObject = true
 				heldObject = collider
+				collider.emit_signal("play_bolt_sound")
 	elif isHoldingObject:
 		isHoldingObject = false
 		heldObject = null
@@ -119,7 +129,7 @@ func maintainInteraction():
 		var forceDirection = marker.global_transform.origin - heldObject.global_transform.origin
 		forceDirection = forceDirection.normalized()
 		
-		heldObject.apply_central_force(forceDirection * 2)
+		heldObject.apply_central_force(forceDirection * 9)
 				
 				
 			
